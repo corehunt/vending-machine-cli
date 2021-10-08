2a67c78fcb561de05664d21fee04e535aa2c0967
Corey Roach <coreyhroach@gmail.com>
2021-10-08 10:46:02 -0400

    push push push

--- src/main/java/com/techelevator/Money.java
+++ src/main/java/com/techelevator/Money.java
@@ -1,7 +1,31 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class Money implements Calculator {
     //private double balance;
+    Scanner keyboard = new Scanner(System.in);
+
+    public Double moneyEntry(){
+            boolean feedMoreMoney = true;
+            while (feedMoreMoney){
+                String input = keyboard.nextLine();
+                Double cashInput = Double.parseDouble(input);
+
+
+                System.out.println("Would you like to add more money? Y/N");
+                String moreMoney = keyboard.nextLine();
+
+                if(moreMoney.equalsIgnoreCase("Y")){
+                    System.out.println("Please a whole dollar amount of $1, $5, or $10.");
+
+                } else if(moreMoney.equalsIgnoreCase("N")){
+                    System.out.println("Total money entered: " + cashInput);
+                    feedMoreMoney = false;
+                }
+
+            }
+        }
 
 
 
---
+++ src/main/java/com/techelevator/UserInteraction.java
@@ -0,0 +1,35 @@
+package com.techelevator;
+
+import java.util.Scanner;
+
+public class UserInteraction {
+    Scanner keyboard = new Scanner(System.in);
+
+
+    public void welcome(){
+        System.out.println("Welcome to Vendomatic 800!");
+        System.out.println("");
+        System.out.println("Press 1 to display vending machine items.");
+        System.out.println("Press 2 to purchase.");
+        System.out.println("Press 3 to exit.");
+        System.out.println("");
+
+        System.out.print("Please make your selection >>> ");
+        String userInput = keyboard.nextLine();
+        if(userInput.equalsIgnoreCase("1")){
+            oneToDisplay();
+        }
+    }
+
+    public void oneToDisplay(){
+        Items vendingItems = new Items();
+        vendingItems.displayItems();
+
+        System.out.println("");
+        System.out.println("Press 2 to purchase.");
+        System.out.println("Press 3 to exit.");
+        String itemSelection = keyboard.nextLine();
+    }
+
+    public void twoToDisplay
+}
